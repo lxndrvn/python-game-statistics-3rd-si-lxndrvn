7db796238e565e996f672b3c8f51a4db926e3971
lxndrvn <i1990522@gmail.com>
2016-11-13 19:10:46 +0100

    import and export

--- part2/export.py
+++ part2/export.py
@@ -1,2 +1,9 @@
+from reports import *
 
-# Export functions
+with open("game_answers.txt", "w+") as input_file:
+    input_file.write("1. " + get_most_played("game_stat.txt") + "\n")
+    input_file.write("2. " + str(sum_sold("game_stat.txt") + "\n"))
+    input_file.write("3. " + str(get_selling_avg("game_stat.txt") + "\n"))
+    input_file.write("4. " + str(count_longest_title("game_stat.txt") + "\n"))
+    input_file.write("5. " + str(get_date_avg("game_stat.txt") + "\n"))
+    input_file.write("6. " + sort_abc("game_stat.txt", "Counter-Strike") + "\n")
--- part2/printing.py
+++ part2/printing.py
@@ -1,2 +1,8 @@
+from reports import *
 
-# Printing functions
+print(get_most_played("game_stat.txt"))
+print(sum_sold("game_stat.txt"))
+print(get_selling_avg("game_stat.txt"))
+print(count_longest_title("game_stat.txt"))
+print(get_date_avg("game_stat.txt"))
+print(get_game("game_stat.txt", "Counter-Strike"))
