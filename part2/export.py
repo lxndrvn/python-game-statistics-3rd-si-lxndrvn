722ab680c32020228930910a980e3bcbd874d80f
lxndrvn <i1990522@gmail.com>
2016-11-14 09:31:29 +0100

    fixed

--- part2/export.py
+++ part2/export.py
@@ -2,8 +2,7 @@ from reports import *
 
 with open("game_answers.txt", "w+") as input_file:
     input_file.write("1. " + get_most_played("game_stat.txt") + "\n")
-    input_file.write("2. " + str(sum_sold("game_stat.txt") + "\n"))
-    input_file.write("3. " + str(get_selling_avg("game_stat.txt") + "\n"))
-    input_file.write("4. " + str(count_longest_title("game_stat.txt") + "\n"))
-    input_file.write("5. " + str(get_date_avg("game_stat.txt") + "\n"))
-    input_file.write("6. " + sort_abc("game_stat.txt", "Counter-Strike") + "\n")
+    input_file.write("2. " + str(sum_sold("game_stat.txt")) + "\n")
+    input_file.write("3. " + str(get_selling_avg("game_stat.txt")) + "\n")
+    input_file.write("4. " + str(count_longest_title("game_stat.txt")) + "\n")
+    input_file.write("5. " + str(get_date_avg("game_stat.txt")) + "\n")
